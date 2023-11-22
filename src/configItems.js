00de55c392d7a2c26ec9598f8c89af1bd4d89901
John Cramer <78001774+CramerJ1470@users.noreply.github.com>
2023-11-21 23:53:25 -0500

    Update configItems.js

--- src/configItems.js
+++ src/configItems.js
@@ -1,3 +1,3 @@
 export const RAPIDAPI_API_FOOTBALL_KEY =
-	"0986cf5021mshf93698741a6b11ap1eec9bjsn92c9211ef7f1";
+	"**************"
 export const RAPIDAPI_ADDRESS = "api-football-v1.p.rapidapi.com";
