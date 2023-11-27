1143073988d4d43091d3487730dbf96b2e5206f1
cramerj1470 <john@Cramerec.com>
2023-11-26 23:05:11 -0500

    data shows in console from Football-API

---
+++ build/precache-manifest.e34dd25cefa03d66a90ee2bb1181bbdb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b600c5cecf5caf69459750dc6ebc1cf1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "25812665df86a054ef9a",
+    "url": "/static/css/main.6c3f24a7.chunk.css"
+  },
+  {
+    "revision": "ef0138280a590887caab",
+    "url": "/static/js/2.b6dab254.chunk.js"
+  },
+  {
+    "revision": "25812665df86a054ef9a",
+    "url": "/static/js/main.5bf9c16a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
