57950b8407249889d08998af45e4f569fc8b92e8
Lisa Miner <midnightmoet@gmail.com>
2023-11-28 17:30:22 -0500

    Making progress

--- src/components/landingPage/landingPage.js
+++ src/components/landingPage/landingPage.js
@@ -3,13 +3,15 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './landing.css'; // Import the specific landing page styles
 import Footer from '../footer/Footer';
+import Navbar from '../navbar/Navbar';
 
 
 function LandingPage() {
   return (
     <div>
+      <Navbar />
       <div className="landing-container">
-        <h1 className="landing-title">Welcome to Sporteka</h1>
+        <h2 className="landing-title">Welcome to Sporteka</h2>
         <p className="landing-description">
           Discover the power of blockchain in the world of sports. Join Sporteka
           today and experience a new era of secure and transparent sports
@@ -17,10 +19,7 @@ function LandingPage() {
         </p>
         <div className="landing-buttons">
           <Link to="/login" className="landing-button">
-            Login
-          </Link>
-          <Link to="/register" className="landing-button">
-            Register
+            Enter Here
           </Link>
         </div>
       </div>
