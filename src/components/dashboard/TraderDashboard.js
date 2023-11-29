9bc57166b6d8f1a9e2043a83aea5f97690f29d07
Lisa Miner <midnightmoet@gmail.com>
2023-11-28 19:56:07 -0500

    Still working on trader dashboard

--- src/components/dashboard/TraderDashboard.js
+++ src/components/dashboard/TraderDashboard.js
@@ -3,11 +3,12 @@ import React, { useState } from "react";
 import "./trader.css"; // Import your CSS file
 
 const TraderDashboard = () => {
-  const [selectedSection, setSelectedSection] = useState("marketingInsights");
+    const [selectedSection, setSelectedSection] = useState("marketingInsights");
 
   // Dummy data for Trader Dashboard
   const traderData = {
-    marketingInsights: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    marketingInsights:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
     betPredictions: "Bet predictions content goes here.",
     notifications: [
       { id: 1, message: "New trade alert: Buy AAPL at $150" },
@@ -19,8 +20,22 @@ const TraderDashboard = () => {
       monthlyEarnings: 1500,
     },
     tradeHistory: [
-      { id: 1, symbol: "AAPL", type: "Buy", quantity: 10, price: 150, date: "2023-01-15" },
-      { id: 2, symbol: "GOOGL", type: "Sell", quantity: 5, price: 2500, date: "2023-02-01" },
+      {
+        id: 1,
+        symbol: "AAPL",
+        type: "Buy",
+        quantity: 10,
+        price: 150,
+        date: "2023-01-15",
+      },
+      {
+        id: 2,
+        symbol: "GOOGL",
+        type: "Sell",
+        quantity: 5,
+        price: 2500,
+        date: "2023-02-01",
+      },
       // Add more trade history entries as needed
     ],
     depositWithdraw: {
@@ -28,8 +43,10 @@ const TraderDashboard = () => {
       totalWithdrawal: 3000,
       availableBalance: 7000,
     },
-    helpSupport: "If you need assistance, please contact our support team at support@example.com.",
+    helpSupport:
+      "If you need assistance, please contact our support team at support@example.com.",
   };
+
 
   return (
     <div className="trader-dashboard">
@@ -38,13 +55,25 @@ const TraderDashboard = () => {
           <img src="profile_pic.jpg" alt="Profile" />
         </div>
         <div className="section-titles">
-          <h4 onClick={() => setSelectedSection("marketingInsights")}>Marketing Insights</h4>
-          <h4 onClick={() => setSelectedSection("betPredictions")}>Bet Predictions</h4>
-          <h4 onClick={() => setSelectedSection("notifications")}>Notifications</h4>
+          <h4 onClick={() => setSelectedSection("marketingInsights")}>
+            Marketing Insights
+          </h4>
+          <h4 onClick={() => setSelectedSection("betPredictions")}>
+            Bet Predictions
+          </h4>
+          <h4 onClick={() => setSelectedSection("notifications")}>
+            Notifications
+          </h4>
           <h4 onClick={() => setSelectedSection("earnings")}>Earnings</h4>
-          <h4 onClick={() => setSelectedSection("tradeHistory")}>Trade History</h4>
-          <h4 onClick={() => setSelectedSection("depositWithdraw")}>Deposit/Withdraw</h4>
-          <h4 onClick={() => setSelectedSection("helpSupport")}>Help/Support</h4>
+          <h4 onClick={() => setSelectedSection("tradeHistory")}>
+            Trade History
+          </h4>
+          <h4 onClick={() => setSelectedSection("depositWithdraw")}>
+            Deposit/Withdraw
+          </h4>
+          <h4 onClick={() => setSelectedSection("helpSupport")}>
+            Help/Support
+          </h4>
         </div>
       </div>
       <div className="main-content">
@@ -52,8 +81,12 @@ const TraderDashboard = () => {
           <h2>Trader Dashboard</h2>
         </div>
         <div className="dashboard-section">
-          {selectedSection === "marketingInsights" && <p>{traderData.marketingInsights}</p>}
-          {selectedSection === "betPredictions" && <p>{traderData.betPredictions}</p>}
+          {selectedSection === "marketingInsights" && (
+            <p>{traderData.marketingInsights}</p>
+          )}
+          {selectedSection === "betPredictions" && (
+            <p>{traderData.betPredictions}</p>
+          )}
           {selectedSection === "notifications" && (
             <ul>
               {traderData.notifications.map((notification) => (
@@ -94,8 +127,13 @@ const TraderDashboard = () => {
           {selectedSection === "depositWithdraw" && (
             <div>
               <p>Total Deposit: ${traderData.depositWithdraw.totalDeposit}</p>
-              <p>Total Withdrawal: ${traderData.depositWithdraw.totalWithdrawal}</p>
-              <p>Available Balance: ${traderData.depositWithdraw.availableBalance}</p>
+              <p>
+                Total Withdrawal: ${traderData.depositWithdraw.totalWithdrawal}
+              </p>
+              <p>
+                Available Balance: $
+                {traderData.depositWithdraw.availableBalance}
+              </p>
             </div>
           )}
           {selectedSection === "helpSupport" && <p>{traderData.helpSupport}</p>}
