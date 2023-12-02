7ebe80f99dbc74e72be14b9a3258d03f4793916f
cramerj1470 <john@Cramerec.com>
2023-12-02 18:38:59 -0500

    mmongodb config for upload

--- config/config.js
+++ config/config.js
@@ -4,7 +4,7 @@ const env = process.env.NODE_ENV || "development";
 const config = {
 	development: {
 		port: process.env.PORT || 443,
-		dbURL: `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster1.iukjd24.mongodb.net/Sporteka`,
+		mongoURI: `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster1.iukjd24.mongodb.net/administrators?retryWrites=true&w=majority`,
 		authCookieName: "x-auth-token",
 	},
 };
