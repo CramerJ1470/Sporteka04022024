1ab3e5c39d0db1d3278480e1df4ea0fe2146f545
cramerj1470 <john@Cramerec.com>
2023-12-02 18:21:17 -0500

    mmongodb config for upload

--- config/config.js
+++ config/config.js
@@ -1,12 +1,17 @@
-require("dotenv").config();
-const env = process.env.NODE_ENV || "development";
+// require("dotenv").config();
+// const env = process.env.NODE_ENV || "development";
 
-const config = {
-	development: {
-		port: process.env.PORT || 443,
-		dbURL: `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster1.iukjd24.mongodb.net/Sporteka`,
-		authCookieName: "x-auth-token",
-	},
-};
+// const config = {
+// 	development: {
+// 		port: process.env.PORT || 443,
+// 		dbURL: `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster1.iukjd24.mongodb.net/Sporteka`,
+// 		authCookieName: "x-auth-token",
+// 	},
+// };
+const db = require("./config/keys").mongoURI;
 
+mongoose
+	.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+	.then(() => console.log("MongoDB connected"))
+	.catch((err) => console.log(err));
 module.exports = config[env];
---
+++ config/key.js
@@ -0,0 +1,4 @@
+module.exports = {
+	mongoURI: `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster1.iukjd24.mongodb.net/administrators?retryWrites=true&w=majority`,
+	jwtSecret: "secret",
+};
