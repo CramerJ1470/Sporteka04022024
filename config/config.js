5e9350fb900e679f576cd00f8467ab2b6c17cfdd
cramerj1470 <john@Cramerec.com>
2023-12-02 18:55:41 -0500

    mmongodb config for upload

--- config/config.js
+++ config/config.js
@@ -1,19 +1,19 @@
 require("dotenv").config();
 const mongoose = require('mongoose');
-const config = require('./config');
-// const env = process.env.NODE_ENV || "development";
+ 
+const env = process.env.NODE_ENV || "development";
 
-// const config = {
-// 	development: {
-// 		port: process.env.PORT || 443,
-// 		mongoURI: `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster1.iukjd24.mongodb.net/administrators?retryWrites=true&w=majority`,
-// 		authCookieName: "x-auth-token",
-// 	},
-// };
-const db = require("./key").mongoURI;
+const config = {
+	development: {
+		port: process.env.PORT || 443,
+		mongoURI: `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster1.iukjd24.mongodb.net/administrators?retryWrites=true&w=majority`,
+		authCookieName: "x-auth-token",
+	},
+};
+// const db = require("./key").mongoURI;
 
-mongoose
-	.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
-	.then(() => console.log("MongoDB connected"))
-	.catch((err) => console.log(err));
+// mongoose
+// 	.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+// 	.then(() => console.log("MongoDB connected"))
+// 	.catch((err) => console.log(err));
 module.exports = config[env];
