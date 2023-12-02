7ee426c1398bdaac8045b200d2d4cabfffd20696
cramerj1470 <john@Cramerec.com>
2023-12-02 18:42:31 -0500

    mmongodb config for upload

--- config/key.js
+++ config/key.js
@@ -1,4 +1,6 @@
-module.exports = () => {
-	return {mongoURI: `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster1.iukjd24.mongodb.net/administrators?retryWrites=true&w=majority`,
-	jwtSecret: "secret",};
+require("dotenv").config();
+
+module.exports = {
+	mongoURI: `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster1.iukjd24.mongodb.net/administrators?retryWrites=true&w=majority`,
+	jwtSecret: "secret",
 };
