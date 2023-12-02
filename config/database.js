0ce303403f4bd4b5c0b3d972a0909bd371639ce7
cramerj1470 <john@Cramerec.com>
2023-12-02 00:26:00 -0500

    defined .env files with heroku config:set <key=value>

--- config/database.js
+++ config/database.js
@@ -2,5 +2,18 @@ const mongoose = require('mongoose');
 const config = require('./config');
 
 module.exports = () => {
-    return mongoose.connect(config.dbURL, { useNewUrlParser: true, useUnifiedTopology: true });
+    return mongoose.connect(config.dbURL, { useNewUrlParser: true, useUnifiedTopology: true },)
+    app.use(express.static(path.join(__dirname, "client", "build")));
+    if (
+    process.env.NODE_ENV === "production" ||
+    process.env.NODE_ENV === "staging"
+  ) {
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "client", "build", "index.html")); 
+    });
+  }
+  
+  app.listen(port, () => {
+    console.log("port 8080..");
+  });;
 };
