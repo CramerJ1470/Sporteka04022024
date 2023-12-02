a7822aa2a736a94f13a6219bd439cac9363352d9
Lisa Miner <midnightmoet@gmail.com>
2023-12-02 12:05:23 -0500

    working on players and player page, not finished

--- src/components/Players.js
+++ src/components/Players.js
@@ -3,25 +3,53 @@ import Player from "./Player";
 import { playersList } from "../assets/players";
 
 function Players() {
-  console.log(playersList);
+  // Define your grid container style
+  const gridContainerStyle = {
+    display: "grid",
+    gap: "10px", // Decreased gap
+    padding: "10px", // Decreased padding
+    border: "1px solid #ccc",
+    marginTop: "20px", // Added top margin
+  };
+
+  // Define your grid item style
+  const gridItemStyle = {
+    background: "#f0f0f0",
+    padding: "10px", // Decreased padding
+    border: "1px solid #ccc",
+    textAlign: "center",
+  };
+
+  // Responsive styles using media queries
+  const responsiveStyles = {
+    "@media (min-width: 768px)": {
+      gridTemplateColumns: "repeat(auto-fill, minmax(200px, 1fr))", // Default for larger screens
+    },
+    "@media (min-width: 1024px)": {
+      gridTemplateColumns: "repeat(auto-fill, minmax(250px, 1fr))", // Increase columns at a larger screen size
+    },
+    "@media (min-width: 1200px)": {
+      gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))", // Further increase columns at an even larger screen size
+    },
+    // Add more media queries as needed
+  };
 
   return (
     <>
-      <div>
-        <div className="Players">
-          <div id="playerBack">
-            {playersList.map((player) => {
-              console.log(player);
-              return <Player key={player.id} player={player.player} />;
-            })}
+      <div className="grid-container" style={{ ...gridContainerStyle, ...responsiveStyles }}>
+        {playersList.map((player) => (
+          <div key={player.id} className="grid-item" style={gridItemStyle}>
+            <Player player={player.player} />
           </div>
-        </div>
+        ))}
       </div>
     </>
   );
 }
 
 export default Players;
+
+
 
 //-------------NOTES-------------------------//
 // 1. Added a `key` prop to the mapped `Player` components to ensure a unique identifier for each player
