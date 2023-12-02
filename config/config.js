5ab1706a9dc91c53cba19dd43c0702a25287e78b
cramerj1470 <john@Cramerec.com>
2023-12-01 23:43:44 -0500

    setting port to 80 for heroku

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ const env = process.env.NODE_ENV || "development";
 
 const config = {
 	development: {
-		port: process.env.PORT,
+		port: process.env.PORT || 80,
 		dbURL: `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster1.nymha.mongodb.net/Pixar1`,
 		authCookieName: "x-auth-token",
 	},
