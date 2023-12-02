404a7e184f72deac9898e1f00cae4f2761284896
cramerj1470 <john@Cramerec.com>
2023-12-02 17:32:44 -0500

    added all data to teams set teams in app.js

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ const env = process.env.NODE_ENV || "development";
 
 const config = {
 	development: {
-		port: process.env.PORT || 8080,
+		port: process.env.PORT || 443,
 		dbURL: `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster1.iukjd24.mongodb.net/Sporteka`,
 		authCookieName: "x-auth-token",
 	},
