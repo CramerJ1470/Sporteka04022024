c36a81fedbbb7fd97e242550088819c62046ca4c
cramerj1470 <john@Cramerec.com>
2023-12-02 19:07:38 -0500

    mmongodb config for upload

--- config/config.js
+++ config/config.js
@@ -7,7 +7,7 @@ const config = {
 		dbURL: `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster1.iukjd24.mongodb.net/Sporteka`,
 		authCookieName: "x-auth-token",
 	},
-// };
+ };
 // const db = require("./key").mongoURI;
 
 // mongoose
--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const config = require('./config');
 
 module.exports = () => {
-    console.log(config.dbURL);
+   
     return mongoose.connect(config.dbURL, { useNewUrlParser: true, useUnifiedTopology: true });
 };
 
