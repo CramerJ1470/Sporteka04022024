173e420d47514e5edb253ca3005ded4b4084af47
cramerj1470 <john@Cramerec.com>
2023-12-02 19:06:07 -0500

    mmongodb config for upload

--- config/config.js
+++ config/config.js
@@ -1,19 +1,17 @@
-require("dotenv").config();
-const mongoose = require('mongoose');
- 
-const env = process.env.NODE_ENV || "development";
+// require("dotenv").config();
+// const env = process.env.NODE_ENV || "development";
 
 const config = {
 	development: {
 		port: process.env.PORT || 443,
-		mongoURI: `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster1.iukjd24.mongodb.net/administrators?retryWrites=true&w=majority`,
+		dbURL: `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster1.iukjd24.mongodb.net/Sporteka`,
 		authCookieName: "x-auth-token",
 	},
-};
+// };
 // const db = require("./key").mongoURI;
 
 // mongoose
 // 	.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
 // 	.then(() => console.log("MongoDB connected"))
 // 	.catch((err) => console.log(err));
-module.exports = config[env];
+// module.exports = config[env];
