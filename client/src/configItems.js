6318567a084e0ea70df9406666a4a101319f7aa6
John Cramer <78001774+CramerJ1470@users.noreply.github.com>
2024-04-04 10:46:40 -0400

    Update configItems.js

--- client/src/configItems.js
+++ client/src/configItems.js
@@ -7,7 +7,7 @@ export const RAPIDAPI_API_FOOTBALL_KEY_BETA =
 	"18cfcb39c4msh2d398b4b4754e0bp105eb5jsn6fd6b1013069";
 export const RAPIDAPI_ADDRESS = "api-football-v1.p.rapidapi.com";
 
-export const SPORTMONKS_API_TOKEN ="ril5QywwUa3Zs3drUJcR7Yo2HDZeaoyjNRsuX5rjYoOu6nMhJuNt5EqiVfff";
+export const SPORTMONKS_API_TOKEN ="ril5QywwUa3Zs3drUJcR7Yo2HDZeaoyjNRsuX5rjYoOu6nMhJuNt5EqiV";
 
 export const SPORTDATA_API_TOKEN ="059fd43194c74d2db03cb4c949c03ce7";
 =======
